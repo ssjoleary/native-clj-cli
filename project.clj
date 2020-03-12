38a5979f53b0cbb3d426db503908f286fe3bc12d
ssjoleary <ssjoleary@gmail.com>
2020-03-12 00:02:53 -0400

    Bump clojure to 1.10.2-alpha1 for graalvm

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
                  [cli-matic "0.3.11"]]
   :repl-options {:init-ns native-clj-cli.core}
   :main native-clj-cli.core
