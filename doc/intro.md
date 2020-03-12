769d13c2f62a7abeb8c147e50267679d26fd405e
ssjoleary <ssjoleary@gmail.com>
2020-03-11 23:14:52 -0400

    Create toycalc CLI

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to native-clj-cli
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/native_clj_cli/core_test.clj
@@ -0,0 +1,6 @@
+(ns native-clj-cli.core-test
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(deftest a-test
+  (testing "FIXME, I do nothing"
+    (is (= 0 0))))
